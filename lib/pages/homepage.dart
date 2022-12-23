6aa102e3acbebc62d8c1700ad6edfb59150e6d1a
Katlego909 <67097039+Katlego909@users.noreply.github.com>
2022-12-23 16:16:04 +0200

    minor changes

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -172,6 +172,10 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
 
+          const SizedBox(
+            height: 12,
+          ),
+
           Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
